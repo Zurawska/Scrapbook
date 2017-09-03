27646860afcaa8f2b9f9ec886f1c8516e657cfa0
Edyta <edyta.zurawska8@gmail.com>
2017-09-03 21:30:45 +0200

    initial commit

---
+++ module8_5JS_Triangle_Area/js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter base a');
+var h = prompt('Enter height');
+var triangleArea = a*h/2;
+alert(triangleArea)
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
